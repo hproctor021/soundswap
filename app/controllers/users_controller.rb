3720c85f5f444753ec78daacbe050f1d15345443
siegelverse <msiegel37@gmail.com>
2020-09-29 17:23:12 -0500

    user/store controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def show 
+        @user = User.find(params[:id])
+    end 
+
+    def edit 
+    end 
+
+    def new 
+        @user = User.new
+    end 
+
+    def create 
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name)
+    end 
 end

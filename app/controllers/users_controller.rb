17314995f3488cbcfba26245d81aa9c485369cb5
hproctor021 <hproctor021@gmail.com>
2020-09-29 19:26:41 -0500

    Added User and Session functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,37 @@
 class UsersController < ApplicationController
     
+    before_aciton :authenticated?, only: [:index, :show]
+
+    def index
+        @user = User.all
+    end
+
+    def create 
+        new_user = User.new(user_params)
+        if new_user.save && params[:password] == params[:password_confirmation] 
+            session[:id] = new_user.id
+            redirect_to user_path(new_user)
+        else
+            flash[:error] = new_user.errors.full_messages
+            redirect_to new_user_path
+        end
+    
+    def authenticated?
+        if session[:id] != nil
+            @user = User.find(session[:id])
+        else
+            redirect_to '/sign_in'
+        end
+    end
+    
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
 end

578cd0b511a7aea0e4c8321d5d6911c0536f67fb
siegelverse <msiegel37@gmail.com>
2020-09-30 16:44:07 -0500

    dynamic rent button

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,15 @@
 class RentalsController < ApplicationController
+    def new 
+        @rental = Rental.new
+    end 
+
+    def create
+        @rental = Rental.create(rental_params)
+        redirect_to user_path(@rental.user_id)
+    end 
+
+    def destroy 
+        @rental = Rental.find(params[:id])
+        @rental.destroy 
+    end 
 end

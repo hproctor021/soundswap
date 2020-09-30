3f0a3108d007cac2bfe31b7640a4d8212bddd7f0
siegelverse <msiegel37@gmail.com>
2020-09-30 14:57:28 -0500

    categories and stores views

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users 
   resources :rentals
+  get '/categories/:category', to: 'categories#show'
+  resources :categories
   resources :instruments
-
-  get '/instruments/:category', to: "instruments#categories", as: instruments_categories
-
   resources :image_element
   resources :stores
 
